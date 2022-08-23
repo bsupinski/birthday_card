eeaadd616a5277bde60aafabf3545c242f0bee69
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2022-08-22 20:03:46 -0400

    Finished base code for balloons

--- index.js
+++ index.js
@@ -1,42 +1,77 @@
-const body = document.querySelector("body")
+const body = document.querySelector("body");
 
+const randomBetween = (min, max) =>
+  Math.floor(Math.random() * (max - min) + min);
 
-const randomBetween = (min, max) => Math.floor(Math.random() * (max - min + 1) + min )
+const randomHSL = () =>
+  `hsl(${randomBetween(0, 360)}, 
+  ${randomBetween(0, 100)}%, 
+  ${randomBetween(0, 100)}%)`;
 
-const randomRGB = () => `rgb(${randomBetween(0, 255)}, ${randomBetween(0, 255)}, ${randomBetween(0, 255)})`
+const fillColor = (element, color) => element.setAttribute("fill", color);
 
-const bgColor = (element, color) => element.style.backgroundColor = color;
-
-
-const randomBalloonColor = (element1, element2) => {
-    const color = randomRGB();
-    bgColor(element1, color);
-    bgColor(element2, color);
-}
-
+const randomBalloonColor = function (element1, element2) {
+  const color = colors[randomBetween(0, colors.length)];
+  fillColor(element1, color[0]);
+  fillColor(element2, color[1]);
+};
 
 const createBalloon = () => {
-    const balloonOuter = document.createElement("div");
-    balloonOuter.classList.add("test1");
-    const balloonInner = document.createElement("div");
-    balloonOuter.style.right=`${randomBetween(0, 100)}%`;
-    balloonInner.classList.add("test2")
-    randomBalloonColor(balloonOuter, balloonInner)
-    balloonOuter.appendChild(balloonInner);
-    body.appendChild(balloonOuter);
+  const balloon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  balloon.classList.add("balloon");
+  balloon.setAttribute("viewBox", "0 0 97 218.02");
 
-    setTimeout(()=>{
-        balloonOuter.remove();
-        balloonInner.remove();
-    }, 5000)
+  const balloonString = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+  balloonString.classList.add("balloon__string");
+  balloonString.setAttribute(
+    "d",
+    "M50.24,119.5c-.46,23.66,1.75,39.64,3.46,48.24,.93,4.7-.4,9.57-3.61,13.05-6.82,7.41-17.45,21.16-15.94,33.55l-.15,3.66"
+  );
 
-}
+  const balloonMain = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+  balloonMain.setAttribute(
+    "d",
+    "M97,59.5c0,29.86-17.93,54.59-41.31,58.85l-1.69,13.65-4-1-4,1s-1.93,0-2-13.26c-.07,0-.14-.01-.21-.02C19.21,115.82,0,90.41,0,59.5,0,26.64,21.71,0,48.5,0s48.5,26.64,48.5,59.5Z"
+  );
 
-const repeatBalloons = () => setInterval(()=> {
-    createBalloon()
+  const balloonHighlight = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+  balloonHighlight.setAttribute(
+    "d",
+    "M82.13,24.19c6.47,9.29,8.99,18.72,5.64,21.05s-7.7-7.45-17.78-12.6c-12.99-6.64-8.99-18.72-5.64-21.05s11.32,3.31,17.78,12.6Z"
+  );
 
-},randomBetween(2000, 6000));
+  randomBalloonColor(balloonMain, balloonHighlight);
+
+  balloon.append(balloonString, balloonMain, balloonHighlight);
+  balloon.style.right = `${randomBetween(0, 100)}%`;
+  return balloon;
+};
+
+const addBalloonDOM = () => {
+  const balloon = createBalloon();
+  body.append(balloon);
+
+  setTimeout(() => {
+    balloon.remove();
+  }, 5000);
+};
+
+const repeatBalloons = () =>
+  setInterval(() => {
+    addBalloonDOM();
+  }, randomBetween(2000, 7000));
 
 repeatBalloons();
 repeatBalloons();
 repeatBalloons();
+
+addBalloonDOM();
