8d9fca6c886b2d780ba952cca4da82c30d33e8e6
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2022-09-05 16:46:06 -0400

    Added first time change
    https://bsupinski.github.io/birthday_card/

--- timer.js
+++ timer.js
@@ -1,7 +1,22 @@
+const overlay = document.querySelector(".timer__overlay");
+
 const days = document.querySelector(".days");
 const hours = document.querySelector(".hours");
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
+
+// Globals
+let daysLeft;
+let hoursLeft;
+let minutesLeft;
+let secondsLeft;
+
+const backGroundChange = (daysLeft) => {
+  if (daysLeft < 10) {
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    return;
+  }
+};
 
 const brittneysBirthday = new Date("September 14, 2022 00:00:00");
 
@@ -11,21 +26,26 @@ const alwaysDoubleDigits = (time, container) => {
     : (container.textContent = time);
 };
 
-const countdown = setInterval(() => {
-  let currentTime = new Date();
-  let distance = brittneysBirthday - currentTime;
+const countdown = () => {
+  setInterval(() => {
+    let currentTime = new Date();
+    distance = brittneysBirthday - currentTime;
 
-  let daysLeft = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hoursLeft = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  let minutesLeft = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
+    daysLeft = Math.floor(distance / (1000 * 60 * 60 * 24));
+    hoursLeft = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    minutesLeft = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
 
-  days.textContent = daysLeft;
-  //   hours.textContent = hoursLeft;
-  alwaysDoubleDigits(daysLeft, days);
-  alwaysDoubleDigits(hoursLeft, hours);
-  alwaysDoubleDigits(minutesLeft, minutes);
-  alwaysDoubleDigits(secondsLeft, seconds);
-}, 1000);
+    days.textContent = daysLeft;
+
+    alwaysDoubleDigits(daysLeft, days);
+    alwaysDoubleDigits(hoursLeft, hours);
+    alwaysDoubleDigits(minutesLeft, minutes);
+    alwaysDoubleDigits(secondsLeft, seconds);
+    backGroundChange(daysLeft);
+  }, 1000);
+};
+
+countdown();
